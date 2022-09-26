114457cb8971a2a1a2a1e4368df420b9682dc651
nikito7 <45373783+nikito7@users.noreply.github.com>
2022-09-26 01:57:30 +0100

    Update eb3-script.js

--- edpbox/eb3-script.js
+++ edpbox/eb3-script.js
@@ -100,6 +100,33 @@ svars
 endif
 
 ; janz wtd
+; janz wtd
+
+clk=s(2.0mm)+s(2.0ss)
+
+if cnt==99
+then
+wtd+=1
+endif
+
+if wtd==1
+then
+old=clk
+endif
+
+if wtd==90
+then
+wtd=0
+if old==clk
+then
+print modbus error !!!
+; 
+=>Restart -3
+; 
+endif
+endif
+
+; janz wtd eof
 ; janz wtd eof
 
 >W
