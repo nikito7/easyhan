c958d01f0d79a7b489b99aa7a93c1a689d541939
nikito7 <45373783+nikito7@users.noreply.github.com>
2022-09-27 10:04:58 +0100

    Update pv1-script.js

--- growatt/pv1-script.js
+++ growatt/pv1-script.js
@@ -13,7 +13,6 @@ m:p:ipwrm=0 6
 m:p:ipwrh=0 60
 m:p:ipwrd=0 24
 ipwr=0
-hour=0
 strh=""
 strd=""
 
@@ -78,12 +77,10 @@ print Saving Vars
 svars
 endif
 
-hour=int(time/60)
-
-if chg[hour]>0
+if chg[hh]>0
 and cnt>30
 then
-strd="cnt"+s(hour)
+strd="cnt"+s(hh)
 ipwrd=ipwrh[-2]
 print Array: ipwrd
 endif
