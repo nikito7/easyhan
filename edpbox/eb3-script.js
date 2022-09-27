dbbd477cc91e5b5b65afe0cd16ba3116293c508e
nikito7 <45373783+nikito7@users.noreply.github.com>
2022-09-27 17:32:27 +0100

    Update eb3-script.js

--- edpbox/eb3-script.js
+++ edpbox/eb3-script.js
@@ -109,33 +109,8 @@ ipwrd=ipwrh[-2]
 print Array: ipwrd
 endif
 
-; janz wtd
-
-clk=s(2.0mm)+s(2.0ss)
-
-if cnt==99
-then
-wtd+=1
-endif
-
-if wtd==1
-then
-old=clk
-endif
-
-if wtd==90
-then
-wtd=0
-if old==clk
-then
-print modbus error !!!
-; 
-=>Restart -3
-; 
-endif
-endif
-
-; janz wtd eof
+; janz wtd begin
+; janz wtd end
 
 >W
 
