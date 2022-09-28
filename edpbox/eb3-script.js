bad058734bf7e9dbbb6412b5eab526475fa04f2a
nikito7 <45373783+nikito7@users.noreply.github.com>
2022-09-28 12:52:43 +0100

    Update eb3-script.js

--- edpbox/eb3-script.js
+++ edpbox/eb3-script.js
@@ -98,7 +98,7 @@ endif
 >W
 
 @<b>NTP </b> %date% %time%
-@<b>Vars </b> cnt=%0cnt% tper=%0tper% smlj=%0smlj%
+@<b>Vars </b> cnt=%0cnt% tper=%0tper% smlj=%0smlj% ver=%0ver%
 @<b>Vars </b> wtd=%0wtd% clk=%0clk% old=%0old%
 @<b>Wifi </b> %wfc% <b> Power </b> %0wfp% <b> Topic </b> %topic%
 @<br>
