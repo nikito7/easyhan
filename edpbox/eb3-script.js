f1c0ee3dd66d543711ac82d93dc6e04cebcd5341
nikito7 <45373783+nikito7@users.noreply.github.com>
2022-09-28 12:09:44 +0100

    Update eb3-script.js

--- edpbox/eb3-script.js
+++ edpbox/eb3-script.js
@@ -102,8 +102,8 @@ print Array: ipwrm %0ipwrm[-1]% %1ipwrm[-2]%
 print Array: ipwrh %0ipwrh[-1]% %1ipwrh[-2]%
 print Array: ipwrd %0ipwrd[-1]% %1ipwrd[-2]%
 ; save to file
-fr=fo("test.txt" 1)
-res=fwa(ipwrm fr)
+fr=fo("test1.txt" 1)
+fwa(ipwrm fr)
 fc(fr)
 endif
 
