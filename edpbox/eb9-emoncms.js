737715b4164fdb0356d7ddbe211fd44f12d428d9
nikito7 <45373783+nikito7@users.noreply.github.com>
2022-09-29 12:44:39 +0100

    Update eb9-emoncms.js

--- edpbox/eb9-emoncms.js
+++ edpbox/eb9-emoncms.js
@@ -1,6 +1,9 @@
 ; emoncms begin
+
 ws="WebSend [10.1.0.79:80]"
 node="EB99"
 key="fa12c4d2ebee895c3cb3b0fedcde35f6"
+
+=>%ws%/input/post.json?json={"ipwr":"%ipwr%","epwr":"%epwr%"}&devicekey=%key%&node=%node%
 
 ; emoncms end
