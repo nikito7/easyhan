d1f32bd26b944e0d3686be07dd08650b4186b13d
nikito7 <45373783+nikito7@users.noreply.github.com>
2022-10-05 16:51:39 +0100

    Update eb9-emoncms.js

--- edpbox/eb9-emoncms.js
+++ edpbox/eb9-emoncms.js
@@ -5,8 +5,9 @@ and cnt>30
 then
 
 ws="WebSend [emoncms.ssn.pt:80]"
-node="EB3"
-key="a01a52cca45abb6fb44331f4d09432ac"
+
+; node.txt
+; key.txt
 
 =>%ws%/input/post.json?json={watts_import:%ipwr%,watts_export:%epwr%,kwh_import:%ikw%,kwh_export:%ekw%}&devicekey=%key%&node=%node%&end=end
 
