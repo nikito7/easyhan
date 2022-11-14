09dd684d355f399ff9323cd9232b15e60ba17a45
nikito7 <45373783+nikito7@users.noreply.github.com>
2022-11-14 21:33:17 +0000

    Update eb9-freeds-wip.js

--- edpbox/eb9-freeds-wip.js
+++ edpbox/eb9-freeds-wip.js
@@ -1,30 +1,9 @@
 ; ---
 
-=>Publish easyhan/pv1/SENSOR {"Time":"%tstamp%",
-"ENERGY":{
-"Pv1Current":1,
-"Pv1Voltage":230.1,
-"Pv1Power":50,
-"Pv2Current":0,
-"Pv2Voltage":0,
-"Pv2Power":0,
-"Today":0,
-"Power":111,
-"Temperature":99}}
+=>Publish easyhan/pv1/SENSOR {"Time":"%tstamp%","ENERGY":{"Pv1Current":1,"Pv1Voltage":230.1,"Pv1Power":50,"Pv2Current":0,"Pv2Voltage":0,"Pv2Power":0,"Today":0,"Power":111,"Temperature":99}}
 
 ; ---
 
-=>Publish easyhan/eb1/SENSOR {"Time":"%tstamp%",
-"ENERGY":{
-"Pv1Current":1,
-"Pv2Current":0,
-"Pv1Voltage":230.1,
-"Pv2Voltage":0,
-"Pv1Power":50,
-"Pv2Power":0,
-"Voltage":230.3,
-"Today":0,
-"Power":111,
-"Temperature":99}}
+=>Publish easyhan/eb1/SENSOR {"Time":"%tstamp%","ENERGY":{"Today":0,"Power":-50}}
 
 ; ---
