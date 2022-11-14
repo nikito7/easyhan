4192ec650d333d001a156cd47675699e13373578
nikito7 <45373783+nikito7@users.noreply.github.com>
2022-11-14 20:53:16 +0000

    Create eb9-freeds-wip.js

---
+++ edpbox/eb9-freeds-wip.js
@@ -0,0 +1,5 @@
+; ---
+
+=>Publish easyhan/pv1/SENSOR {"Time":"%tstamp%","ENERGY":{"Pv1Current":1,"Pv2Current":0,"Pv1Voltage":230.1,"Pv2Voltage":0,"Pv1Power":50,"Pv2Power":0,"Voltage":230.3,"Today":0,"Power":111,"Temperature":99}}
+
+; ---
