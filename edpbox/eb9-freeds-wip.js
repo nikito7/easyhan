278016693521f496095981fcffa6bb57ee08c73f
nikito7 <45373783+nikito7@users.noreply.github.com>
2022-11-14 22:56:30 +0000

    Update and rename eb9-freeds-wip.js to eb9-freeds-test.js

--- edpbox/eb9-freeds-wip.js
+++
@@ -1,3 +0,0 @@
-; ---
-=>Publish easyhan/edpbox/SENSOR {"Time":"%tstamp%","ENERGY":{"Pv1Current":0,"Pv1Voltage":0,"Pv1Power":"%epwr%","Pv2Current":0,"Pv2Voltage":0,"Pv2Power":0,"Today":0,"Power":"%ipwr%","Temperature":99}}
-; ---
