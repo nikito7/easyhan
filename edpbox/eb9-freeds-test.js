e41cab5b5dd9a5626641e508a272bd1156842b77
nikito7 <45373783+nikito7@users.noreply.github.com>
2022-11-15 00:51:43 +0000

    Update eb9-freeds-test.js

--- edpbox/eb9-freeds-test.js
+++ edpbox/eb9-freeds-test.js
@@ -1,3 +1,3 @@
 ; ---
-=>Publish easyhan/eb/SENSOR {"Time":"%tstamp%","ENERGY":{"Pv1Current":0,"Pv2Current":0,"Pv1Voltage":0,"Pv2Voltage":0,"Pv1Power":0,"Pv2Power":0,"Today":0,"Power":%0ipwr%,"Temperature":99}}
+=>Publish easyhan/eb/SENSOR {"Time":"%tstamp%","ENERGY":{"Pv1Current":0,"Pv2Current":0,"Pv1Voltage":0,"Pv2Voltage":0,"Pv1Power":%0ipwr%,"Pv2Power":0,"Today":0,"Power":%0ipwr%,"Temperature":99}}
 ; ---
