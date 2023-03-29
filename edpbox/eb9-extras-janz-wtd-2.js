b567523eef247d059b5662c5575fb416a5c69f4d
nikito7 <45373783+nikito7@users.noreply.github.com>
2023-03-29 18:55:59 +0100

    Update eb9-extras-janz-wtd-2.js

--- edpbox/eb9-extras-janz-wtd-2.js
+++ edpbox/eb9-extras-janz-wtd-2.js
@@ -1,27 +1,24 @@
-; ---
+; --
 
 clk=s(2.0mm)+s(2.0ss)
 
-if cnt==99
-{
+if cnt==99 {
 wtd+=1
 }
 
-if wtd==1
-{
+if wtd==1 {
 old=clk
 }
 
-if wtd==30
-{
+if wtd==30 {
 wtd=0
 ;
-if old==clk
-{
+if old==clk {
 cnt=1
-=>BackLog Script 0; Delay 500; Script 1; Sensor53 r
+=>Publish2 HAN/%EB%/WDT %tstamp%
+=>BackLog Script 0; Delay 300; Script 1; Sensor53 r
 }
 ;
 }
 
-; ---
+; --
