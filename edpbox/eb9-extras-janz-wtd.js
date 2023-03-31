f158c43a8b5409803e1868b6d157d7b5a2ff8cdf
nikito7 <45373783+nikito7@users.noreply.github.com>
2023-03-31 21:41:39 +0100

    Update eb9-extras-janz-wtd.js

--- edpbox/eb9-extras-janz-wtd.js
+++ edpbox/eb9-extras-janz-wtd.js
@@ -1,4 +1,4 @@
-; ---
+; --
 
 clk=s(2.0mm)+s(2.0ss)
 
@@ -12,15 +12,16 @@ if wtd==1
 old=clk
 }
 
-if wtd==300
+if wtd==120
 {
 wtd=0
 ;
 if old==clk
 {
+=>Publish2 HAN/%EB%/WTD %tstamp%
 =>Restart -3
 }
 ;
 }
 
-; ---
+; --
