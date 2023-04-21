49298e68cb1c7cbdbabf84c949ed17457f60a64e
nikito7 <45373783+nikito7@users.noreply.github.com>
2023-04-21 16:22:38 +0100

    Update eb9-freeds-test.js

--- edpbox/eb9-freeds-test.js
+++ edpbox/eb9-freeds-test.js
@@ -1,9 +1,10 @@
 ; ---
+; fake solar
 tmp=0
 tmp=ipwr*4
-=>Publish easyhan/ds_pv/SENSOR {"Time":"%tstamp%","ENERGY":{"Pv1Current":0,"Pv2Current":0,"Pv1Voltage":0,"Pv2Voltage":0,"Pv1Power":0,"Pv2Power":0,"Today":0,"Power":%0tmp%,"Temperature":0}}
-; ---
+=>Publish HAN/%EB%/ds_pv/SENSOR {"Time":"%tstamp%","ENERGY":{"Pv1Current":0,"Pv2Current":0,"Pv1Voltage":0,"Pv2Voltage":0,"Pv1Power":0,"Pv2Power":0,"Today":0,"Power":%0tmp%,"Temperature":0}}
+; edpbox
 tmp=ipwr*-1
-=>Publish easyhan/ds_eb/SENSOR {"Time":"%tstamp%","ENERGY":{"Voltage":0,"Power":%0tmp%}}
+=>Publish HAN/%EB%/ds_eb/SENSOR {"Time":"%tstamp%","ENERGY":{"Voltage":0,"Power":%0tmp%}}
 tmp=0
 ; ---
