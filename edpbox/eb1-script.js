ad743c4709761593dae34ed55957d7f09736b026
nikito7 <45373783+nikito7@users.noreply.github.com>
2023-05-14 17:44:03 +0100

    Update eb1-script.js

--- edpbox/eb1-script.js
+++ edpbox/eb1-script.js
@@ -367,10 +367,10 @@ $gc(e)
 1,01441dx4xxxxuu@i7:1,LP Minute,m,LP1_MM,0
 ; summer
 ; amr
-1,01441dx8x4xxUUuuUUuu@i7:1000,LP Import Inc,kWh,LP3_IMP,0
+1,01441dx8x4xxUUuuUUuu@i7:1000,LP Import Inc,kWh,LP3_IMP,3
 ; lp4
 ; lp5
-1,01441dx8x8x8xxUUuuUUuu@i7:1000,LP Export Inc,kWh,LP6_EXP,0
+1,01441dx8x8x8xxUUuuUUuu@i7:1000,LP Export Inc,kWh,LP6_EXP,3
 
 #
 ; eof
