867a4fec71b860c18c77be3e7c1b08b1008b1c46
nikito7 <45373783+nikito7@users.noreply.github.com>
2023-05-15 17:14:21 +0100

    Update eb3-script.js
    Wh > kWh bug fix

--- edpbox/eb3-script.js
+++ edpbox/eb3-script.js
@@ -1,6 +1,6 @@
 >D 48
 
-ver=10281
+ver=10282
 EB="EB3"
 C="Calculado"
 PF="Factor"
@@ -234,7 +234,7 @@ lpf="LP-"+s(4.0lp1y)+"-"+s(2.0lp1m)+".csv"
 if chg[lp1mm]>0
 and cnt>50
 then
-lps=s(4.0lp1y)+"-"+s(2.0lp1m)+"-"+s(2.0lp1d)+"T"+s(2.0lp1hh)+":"+s(2.0lp1mm)+","+s(0lp3i)+","+s(0lp6e)+"\n"
+lps=s(4.0lp1y)+"-"+s(2.0lp1m)+"-"+s(2.0lp1d)+"T"+s(2.0lp1hh)+":"+s(2.0lp1mm)+","+s(3lp3i)+","+s(3lp6e)+"\n"
 ;
 fr=fo(lpf 2)
 ;
