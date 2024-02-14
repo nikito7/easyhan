e14ebd3837b27d7b58f684fc33a9ae6d084c2412
nikito7 <45373783+nikito7@users.noreply.github.com>
2024-02-14 21:31:43 +0000

    Create eb1-script.js

---
+++ V2/eb1-script.js
@@ -0,0 +1,279 @@
+>D 48
+
+ver=20002
+C="Net."
+date=""
+time=""
+clk=""
+old=""
+wfc=""
+wfp=0
+cnt=0
+wtd=0
+hh=0
+mm=0
+ss=0
+thh=0
+tmm=0
+;
+m:ipwrm=0 50
+m:epwrm=0 50
+ipwr=0
+epwr=0
+strm="cnt0"
+fheap=0
+;
+p:ikw=0
+p:ekw=0
+fr=0
+res=0
+;
+lp1y=0
+lp1m=0
+lp1d=0
+lp1hh=0
+lp1mm=0
+lp3i=0
+lp6e=0
+lps=""
+lpf=""
+;
+p:ikwo=0
+p:ekwo=0
+tmp=0
+chs=""
+chf=""
+imp=0
+exp=0
+;
+saldo=0
+p:saldo1=0
+p:saldo2=0
+;
+vts=""
+vtf=""
+vt1=0
+vt2=0
+vt3=0
+
+>B
+
+spinm(2 1)
+
+tper=20
+smlj=0
+
+=>SerialLog 0
+
+>E
+
+wfc=WifiConfig#?
+wfp=WifiPower
+
+>T
+
+ipwr=EB1#API
+epwr=EB1#APE
+ikw=EB1#TEI
+ekw=EB1#TEE
+lp1y=EB1#LP1_Y
+lp1m=EB1#LP1_M
+lp1d=EB1#LP1_D
+lp1hh=EB1#LP1_HH
+lp1mm=EB1#LP1_MM
+lp3i=EB1#LP3_IMP
+lp6e=EB1#LP6_EXP
+vt1=EB1#VL1
+vt2=EB1#VL2
+vt3=EB1#VL3
+hh=EB1#HH
+mm=EB1#MM
+ss=EB1#SS
+
+>S
+
+spin(2 0)
+delay(100)
+spin(2 1)
+
+time=st(tstamp T 2)
+date=st(tstamp T 1)
+fheap=heap/1024
+
+if cnt==21
+then
+tper=11
++>WifiConfig
++>WifiPower
++>BackLog Script 8
+endif
+
+if cnt==31
+then
+=>UfsRun discovery-EB1.txt
+endif
+
+if cnt<99
+then
+cnt+=1
+print cnt=%0cnt%
+endif
+
+if chg[ss]>0
+and cnt>30
+then
+ipwrm=ipwr
+epwrm=epwr
+; freeds
+endif
+
+if upsecs%600==0
+and cnt>30
+then
+;
+vts=s(2.0hh)+":"+s(2.0mm)+","+s(0vt1)+","+s(0vt2)+","+s(0vt3)+"\n"
+vtf="vt-0d.csv"
+fr=fo(vtf 2)
+res=fz(fr)
+if res==0
+then
+;
+if EB=="EB1"
+then
+res=fw(date+",L1\n" fr)
+else
+res=fw(date+",L1,L2,L3\n" fr)
+endif
+;
+fc(fr)
+fr=fo(vtf 2)
+endif
+res=fw(vts fr)
+fc(fr)
+;
+endif
+
+strd="cnt"+s(hh)
+
+if chg[hh]>0
+and cnt>50
+then
+;
+if hh==0
+then
+=>UfsDelete2 2d.csv
+=>UfsRename2 1d.csv,2d.csv
+=>UfsRename2 0d.csv,1d.csv
+;
+=>UfsDelete2 vt-2d.csv
+=>UfsRename2 vt-1d.csv,vt-2d.csv
+=>UfsRename2 vt-0d.csv,vt-1d.csv
+;
+tmp=lp1y-1
+=>UfsDelete2 LP-%4.0tmp%-%2.0lp1m%.csv
+;
+endif
+;
+if ikwo==0
+or ikw==0
+then
+ikwo=ikw
+endif
+;
+if ekwo==0
+or ekw==0
+then
+ekwo=ekw
+endif
+;
+imp=ikw-ikwo
+exp=ekw-ekwo
+;
+chs=s(2.0hh)+"h"+","+s(imp)+","+s(exp)+"\n"
+chf="0d.csv"
+fr=fo(chf 2)
+res=fz(fr)
+if res==0
+then
+res=fw(date+",Import,Export\n" fr)
+fc(fr)
+fr=fo(chf 2)
+endif
+res=fw(chs fr)
+fc(fr)
+;
+ikwo=ikw
+ekwo=ekw
+svars
+;
+endif
+
+lpf="LP-"+s(4.0lp1y)+"-"+s(2.0lp1m)+".csv"
+
+if chg[lp1mm]>0
+and cnt>50
+then
+lps=s(4.0lp1y)+"-"+s(2.0lp1m)+"-"+s(2.0lp1d)+"T"+s(2.0lp1hh)+":"+s(2.0lp1mm)+","+s(3lp3i)+","+s(3lp6e)+"\n"
+;
+fr=fo(lpf 2)
+;
+res=fz(fr)
+if res==0
+then
+res=fw("Date,Import,Export\n" fr)
+fc(fr)
+fr=fo(lpf 2)
+endif
+;
+; LP csv
+res=fw(lps fr)
+;
+fc(fr)
+;
+saldo=lp3i-lp6e
+if saldo>0
+then
+saldo1+=saldo
+endif
+if saldo<0
+then
+saldo2+=saldo*-1
+endif
+svars
+;
+endif
+
+; extras
+; extras
+
+>J
+,"HAN":{
+"s0":%3saldo%,
+"s1":%3saldo1%,
+"s2":%3saldo2%,
+"ck":"%2.0hh%:%2.0mm%:%2.0ss%"
+}
+
+>W
+
+@<b>NTP </b> %date% %time% <b> Heap </b> %1fheap%
+@<b>Vars </b> cnt=%0cnt% tper=%0tper% ver=%0ver%
+@<b>Wifi </b> %wfc% <b> Power </b> %0wfp% <b> Topic </b> %topic%
+@<br>
+<br>
+Consumo %C%{m}%3saldo1% kWh
+Excedente %C%{m}%3saldo2% kWh
+<br>
+<a href="/ufs/%lpf%">%lpf%</a>{m}<a href="/ufs/charts.html">Charts</a>
+<br>
+
+$<div id="chart1" style="width:95%%;height:250px;padding:0px;"></div><br><br>
+$gc(lt ipwrm epwrm "wr" "Import" "Export" strm)
+$var options = {
+$chartArea:{left:50,width:'80%%'},
+$width:'100%%',legend:'none',
+$title:'Potência (W) (10min)',
+$};
+$gc(e)
+
+; EOF V2
