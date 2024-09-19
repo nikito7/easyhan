f179db402123deb46057015d4b735261e300cf4e
nikito7 <45373783+nikito7@users.noreply.github.com>
2024-09-19 21:46:23 +0100

    Update eb9-freeds-test.js

--- V2/eb9-freeds-test.js
+++ V2/eb9-freeds-test.js
@@ -6,6 +6,8 @@ tmp=0
 tmp=300
 =>Publish freeds/fake_solar/SENSOR {"Time":"%tstamp%","ENERGY":{"Pv1Current":0,"Pv2Current":0,"Pv1Voltage":0,"Pv2Voltage":0,"Pv1Power":0,"Pv2Power":0,"Today":0,"Power":%0tmp%,"Temperature":0}}
 ; edpbox
+; ipwr import
+; epwr export
 tmp=ipwr-tmp
 =>Publish freeds/%EBx%/SENSOR {"Time":"%tstamp%","ENERGY":{"Voltage":%1vt1%,"Power":%0tmp%}}
 tmp=0
