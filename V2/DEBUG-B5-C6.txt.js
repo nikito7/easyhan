1755760271522de1a17b4bd51c7553e04f68b458
nikito7 <45373783+nikito7@users.noreply.github.com>
2025-03-22 18:26:28 +0000

    Create DEBUG-B5-C6.txt.js

---
+++ V2/DEBUG-B5-C6.txt.js
@@ -0,0 +1,35 @@
+>D
+>B
+
+=>SerialLog 0
+=>Sensor53 r
+=>Sensor53 d0
+
+>M 1
+
+; esp32 19/18
+; esp8266 3/1
+; C6 pins 4 5(3)
+; change to your gpios and mode
+; power off is required
+; rx mode  baud     tx time
+;  v  v     v        v  v
++1,4,mN1,1,9600,Test,5(3),30,r010400010001
+
+
+; console debug
+;
+; sensor53 d1
+; sensor53 d0
+
+1,=h<hr>
+
+1,01040Cxxxxxxxxxxuu@i0:1,Clock ,h,*,0
+1,01040Cxxxxxxxxxxxxuu@i0:1,Clock ,m,*,0
+1,01040Cxxxxxxxxxxxxxxuu@i0:1,Clock ,s,*,0
+
+1,=h<hr>
+
+#
+
+; eof
