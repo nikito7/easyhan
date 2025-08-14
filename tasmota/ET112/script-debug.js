1bef7964f7457dde31d230b4cbf2efe26609ed5a
nikito7 <45373783+nikito7@users.noreply.github.com>
2025-08-14 12:12:32 +0100

    Update script-debug.js

--- tasmota/ET112/script-debug.js
+++ tasmota/ET112/script-debug.js
@@ -1,4 +1,6 @@
->D 48
+>D 128
+res=0
+
 >B
 
 =>SerialLog 0
@@ -15,7 +17,9 @@
 ; power off is required
 ;  rx mode  baud     tx dirtime
 ;   v  v     v        v  v
-+1,17,mN1,1,9600,ET112,15(16),30,r010300000010
++1,17,mN1,1,9600,ET112,15(16),30,r010300000010,r010301000012,r01030112000C,r010311030001,r0103000B0001
+
+;,r010611030001
 
 
 ; console debug
@@ -25,17 +29,61 @@
 
 1,=h<hr>
 
-1,010320SSss@i0:10,Volts,V,*,1
-1,010320x2SSssSSss@i0:1000,Amps,A,*,3
-1,010320x6SSssSSss@i0:10,Watts,W,*,1
+1,=hTable 1
+
+1,010320SSss@i0:10,Volts,V,Voltage,1
+1,010320x2SSssSSss@i0:1000,Amps,A,Current,3
+1,010320x6SSssSSss@i0:10,Watts,W,Power,1
 1,010320x10SSssSSss@i0:10,VA,VA,*,1
 1,010320x14SSssSSss@i0:10,VAr,VAr,*,1
 1,010320x18SSssSSss@i0:10,W dmd,,*,1
 1,010320x22SSssSSss@i0:10,W dmd peak,,*,1
-1,010320x26SSssSSss@i0:1000,PF,,*,3
-1,010320x30SSss@i0:10,Freq,Hz,*,1
+1,010320x26SSssSSss@i0:1000,PF,,PowerFactor,3
+1,010320x30SSss@i0:10,Freq,Hz,Frequency,1
+
+1,=h<hr>
+
+1,=hTable 2 A
+
+1,010324SSssSSsss@i1:1000,Amps,A,*,3
+1,010324x4SSssSSsss@i1:10,Volts,V,*,1
+
+1,010324x8SSssSSsss@i1:10,Null,,*,0
+
+1,010324x12SSssSSsss@i1:10,Watts,W,*,1
+1,010324x16SSssSSsss@i1:10,VA,VA,*,1
+1,010324x20SSssSSsss@i1:10,VAr,VAr,*,1
+
+1,010324x24SSssSSsss@i1:1000,PF,,*,3
+1,010324x28SSssSSsss@i1:10,Null,,*,0
+1,010324x32SSssSSsss@i1:10,Freq,Hz,*,1
+
+1,=h<hr>
+
+1,=hTable 2 B
+
+1,010318SSssSSsss@i2:10,kWh+ total,,*,1
+1,010318x4SSssSSsss@i2:10,kVArh+ total,,*,1
+1,010318x8SSssSSsss@i2:10,kWh- total,,*,1
+1,010318x12SSssSSsss@i2:10,kVArh- total,,*,1
+
+1,010318x16SSssSSsss@i2:10,W dmd,,*,1
+1,010318x20SSssSSsss@i2:10,W dmd peak,,*,1
+
+
+1,=h<hr>
+
+1,=hDevice
+
+1,010302UUuu@i3:1,Mode,,Mode,0
+1,010302UUuu@i4:1,Model,,Model,0
+
+
+;1,0106xxxxxxuu@i5:1,SetMode Debug,,SetMode,0
+
 1,=h<hr>
 
 #
 
 ; eof
+
