e3ec0f582a3635c6eecaae607b6458c7b9eeeede
nikito7 <45373783+nikito7@users.noreply.github.com>
2026-06-20 14:56:06 +0100

    Update Derivadores.txt.js

--- V2/Derivadores.txt.js
+++ V2/Derivadores.txt.js
@@ -9,9 +9,10 @@ tper=60
 
 >S
 
-if cnt==10
+if cnt==20
 {
-+>BackLog Script 8
+=>BackLog Script 8
+=>HanSkip 1,1,1,1,1,1,0,1,1,1,1,1,1,1,1
 }
 
 if cnt<100
@@ -19,11 +20,12 @@ if cnt<100
 cnt+=1
 }
 
+if (upsecs%300==0 and upsecs>100)
+{
+=>BackLog HanSkip 0,0; Delay 200; HanSkip 1,1,1,1,1,1,0,1,1,1,1,1,1,1,1
+}
 
 ; EOF V2
-
-
-
 
 
 
